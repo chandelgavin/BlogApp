7dd67e666cb2a04647bc1e1bf351a40143ab1b34
chandelgavin <124235869+chandelgavin@users.noreply.github.com>
2025-01-01 20:17:06 +0530

    Delete to update src directory

--- src/main/java/com/example/blogapp/articles/ArticleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.blogapp.articles;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ArticleRepository extends JpaRepository<ArticleEntity, Long> {
-}
