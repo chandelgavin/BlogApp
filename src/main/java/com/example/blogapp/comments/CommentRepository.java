f10cbec1bda2af5e61a290dd22c4d47f90850784
chandelgavin <124235869+chandelgavin@users.noreply.github.com>
2025-01-06 12:59:14 +0530

    Delete src/main/java/com/example/blogapp directory

--- src/main/java/com/example/blogapp/comments/CommentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.blogapp.comments;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommentRepository extends JpaRepository<CommentEntity, Long> {
-}
